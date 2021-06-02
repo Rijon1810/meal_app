f45cfde308a921cd4b7d64e8c8882641549c7f8e
Rijon <rijon.r@yahoo.com>
2021-06-02 08:43:57 +0600

    morning commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:meal_app/screens/category_meals_screen.dart';
+import 'package:meal_app/screens/filters_screen.dart';
 import 'package:meal_app/screens/meal_detail_screen.dart';
 import 'package:meal_app/screens/tabs_screen.dart';
+
 import 'screens/categories_screen.dart';
 
 void main() => runApp(MyApp());
@@ -36,6 +38,7 @@ class MyApp extends StatelessWidget {
         '/': (ctx) => TasbsScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+        FiltersScreen.routeName: (ctx) => FiltersScreen(),
       } /* ,
       onGenerateRoute: (settings){
         print(settings.arguments);
